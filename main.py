769b1d822c451f9d1057e17ebe51a6fd7e8a065c
rafalwinnik0 <130064427+rafalwinnik0@users.noreply.github.com>
2023-05-13 15:24:15 +0200

    initial commit

--- main.py
+++ main.py
@@ -18,20 +18,3 @@ with webdriver.Chrome(service=service) as driver:
             "name": name_table[n].text
         }
 
-print(dict)
-
-    # name_table = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
-    # for name in name_table:
-    #     events.append(name.text)
-
-
-# table1 = ['2023-05-17', '2023-05-17', '2023-05-25', '2023-05-27', '2023-05-29']
-# table2 = ['KX CON [23]', 'PyCon LT 2023', 'PyCon Italia 2023', 'Django Girls Groningen', 'DjangoCon Europe 2023']
-# events = {}
-#
-# for n in range(len(table1)):
-#     events[n] = {
-#         "date": table1[n],
-#         "name": table2[n],
-#     }
-# print(events)
